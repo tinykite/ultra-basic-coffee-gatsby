c73c3d023761c9a83e86516299ad68d10089177c
tinykite <dakota@tinykitelab.com>
2020-02-23 10:18:53 -0600

    Add gatsby image transform loader plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,8 @@ module.exports = {
         },
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
